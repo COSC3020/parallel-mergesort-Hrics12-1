d31a99695b9f13082ba3f9c771465821c1e89f78
Hrics12 <111898293+Hrics12@users.noreply.github.com>
2024-12-07 10:29:00 -0700

    Create code.test.js

---
+++ code.test.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const jsc = require('jsverify');
+
+eval(fs.readFileSync('code.js') + '');
+
+// Test to verify that parallel mergesort produces the same results as the built-in sort
+const testSort = jsc.forall("array nat", async function(arr) {
+    // Make two copies of the array
+    var a1 = JSON.parse(JSON.stringify(arr));
+    var a2 = JSON.parse(JSON.stringify(arr));
+
+    // Sort using parallel mergesort
+    await parallelMergesort(a1);
+
+    // Sort using built-in JavaScript sort
+    a2.sort(function(a, b) { return a - b; });
+
+    // Compare both sorted arrays
+    return JSON.stringify(a1) === JSON.stringify(a2);
+});
+
+// Assert the test
+jsc.assert(testSort);
